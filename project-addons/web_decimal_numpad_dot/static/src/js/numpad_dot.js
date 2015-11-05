7aa4081aa4bcc616a201bc6c75971ddaa6b09642
Omar <omarcs7r@gmail.com>
2015-11-05 14:54:54 +0100

    [ADD] web_decimal_numpad_dot: Nuevo módulo que permite usar el tecñado numérico para meter cantidades con decimales

---
+++ project-addons/web_decimal_numpad_dot/static/src/js/numpad_dot.js
@@ -0,0 +1,21 @@
+(function() {
+
+var instance = openerp;
+
+instance.web.form.FieldFloat = instance.web.form.FieldChar.extend({
+    is_field_number: true,
+    widget_class: 'oe_form_field_float',
+    events: {
+        "keypress": "floatKeypress",
+    },
+    floatKeypress: function(e){
+        if(e.keyCode == '46' || e.charCode == '46'){
+            //Cancel the keypress
+            e.preventDefault();
+            // Add the comma to the value of the input field
+             this.$("input").val(this.$("input").val() + ',');
+         }
+    },
+});
+
+})();
