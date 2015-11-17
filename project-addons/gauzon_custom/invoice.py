e3d4c44a57f409a587e6ec801aa2dce1607dcf37
Omar <omarcs7r@gmail.com>
2015-11-17 18:55:33 +0100

    [REF] gauzon_custom: Truncamos a 64 caracteres el nombre y origen de las facturas porque sino no permite editar la factura facilmente porque ocupa mucho la cabecera

--- project-addons/gauzon_custom/invoice.py
+++ project-addons/gauzon_custom/invoice.py
@@ -1,29 +1,30 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    Copyright (C) 2004-2011 Pexego Sistemas Informáticos. All Rights Reserved
-#    $Javier Colmenero Fernández$
-#
+#    Copyright (C) 2015 Comunitea All Rights Reserved
+#    $Omar Castiñeira Saavedra <omar@comunitea.com>$
 #
 #    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
 #    (at your option) any later version.
 #
 #    This program is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
+#    GNU Affero General Public License for more details.
 #
-#    You should have received a copy of the GNU General Public License
+#    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
 
-import account_move
-import analytic_line
-import product
-import partner
-import purchase
-import sale
-import invoice
+from openerp import models, fields
+
+
+class AccountInvoice(models.Model):
+
+    _inherit = "account.invoice"
+
+    name = fields.Char(size=64)
+    origin = fields.Char(size=64)
