7b6b91a7fd6f3686123200f15c7d2c53917b2e72
Omar (Comunitea) <omar@comunitea.com>
2021-01-18 12:30:01 +0100

    [UPD] buidout. [FIX] gauzon_custom: import faltante

--- project-addons/gauzon_custom/models/hr_holidays.py
+++ project-addons/gauzon_custom/models/hr_holidays.py
@@ -1,7 +1,7 @@
 # © 2020 Comunitea Servicios Tecnológicos S.L. (https://comunitea.com)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields
+from odoo import api, models, fields
 
 
 class Holidays(models.Model):
